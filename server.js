6e54b7dd9e7380f3fa0acd551941d8b52a900ae5
GreatnessAwaits <104642097+RedHawk-1@users.noreply.github.com>
2025-03-30 19:17:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '/')));
 
 // Handle all routes by serving index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'gen.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
